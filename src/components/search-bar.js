328d6224e556aba41ca27c271cef99798f7e1560
Vincent <scrapp3rz@gmail.com>
2018-07-18 16:49:44 +0200

    début videoListItem

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const SearchBar = function() {
-    return <input type="text" placeholder="Recherche"/>
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchText: "", placeHolder: "votre recherche" };
+  }
+  render() {
+   
+      return(
+           <div>
+      <input onChange={this.handleChange.bind(this)} placeholder={this.placeHolder} />
+ 
+    </div>
+      )
+  }
+
+  handleChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
 }
 
 export default SearchBar;
